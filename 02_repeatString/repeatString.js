b8255e94b55d4de9257e82b4633ad942c2ffcbe9
Alex Hwang <alexhwang01134@gmail.com>
2023-09-25 11:57:08 -0400

    Completed exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, numOfRepeats) {
+    let output = '';
 
+    if (numOfRepeats < 0 ) {
+        return 'ERROR';
+    } else if (numOfRepeats == 0) {
+        return output;
+    }
+    for (let i = 0; i < numOfRepeats; i++) {
+         output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
