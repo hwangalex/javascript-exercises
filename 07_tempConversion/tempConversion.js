4e2d6c708b059cff031dd0d2a5f48f66741ba972
Alex Hwang <alexhwang01134@gmail.com>
2023-09-28 14:53:52 -0400

    Completed exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+  let conversion = (F-32) * 5/9;
+
+  if (F == 32) {
+    return 0;
+  } else {
+    return Math.round(conversion * Math.pow(10, 1)) / Math.pow(10, 1);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  let conversion = (C*9/5) + 32;
+
+  if (C == 0) {
+    return 32;
+  } else {
+    return Math.round(conversion * Math.pow(10, 1)) / Math.pow(10, 1);
+  }
 };
 
 // Do not edit below this line
