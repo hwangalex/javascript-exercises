2ce9874a252407618632b5b1c76517dff9e70a3e
Alex Hwang <alexhwang01134@gmail.com>
2023-10-04 17:16:10 -0400

    Completed exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let convertableString = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    let splitString = convertableString.split("");
+    if (splitString.reverse().join("") == convertableString) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
