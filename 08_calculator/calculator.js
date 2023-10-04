53f9fbe8f1ee2ebed505aec2037ac0a1fe1e1406
Alex Hwang <alexhwang01134@gmail.com>
2023-10-04 15:38:51 -0400

    Completed exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+  for (let num in nums) {
+    sum += nums[num];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let outcome = 1;
+  for (let num in nums) {
+    outcome *= nums[num]
+  }
+  return outcome;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+  let current = num;
+  for (let i = 0; i < exponent - 1; i++) {
+    current *= num;
+  }
+  return current;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let current = 1;
+
+  if (num == 0) {
+    return 1;
+  } else {
+    for (let i = num; i > 0; i--) {
+      current *= num;
+      num -= 1;
+    }
+  }
+
+  return current;
 };
 
 // Do not edit below this line
