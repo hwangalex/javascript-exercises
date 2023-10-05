690c2c6f76123fdb3108463061defd6851948fc2
Alex Hwang <alexhwang01134@gmail.com>
2023-10-05 11:46:55 -0400

    Completed exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let num = Number(number);
+    let holder1 = 1;
+    let holder2 = 0;
 
+    if (num < 0) {
+        return "OOPS";
+    } else {
+        for (i = 2; i <= num; i++) {
+            let current = holder1 + holder2;
+            holder2 = holder1;
+            holder1 = current;
+        }
+
+        return holder1;
+    }
 };
 
 // Do not edit below this line
